a0e4e1a93f223f1befc7520a9983523e80686138
alexpg40 <alexpg40@educantabria.es>
2021-12-18 15:17:00 +0100

    ya se crean y se muestran los registros de cada usuario

--- javascript/adminUser.js
+++ javascript/adminUser.js
@@ -1,14 +1,66 @@
-document.onload = () => {
-    crearUsuarios();
+window.onload = () => {
+    recuperarUsuarios();
 }
 
-const crearUsuarios = () => {
-    let adminUser = document.getElementsByClassName('wrapper__admin-users');
-    let todosUsuarios = recuperarUsuarios();
+const crearUsuarios = (todosUsuarios) => {
+    let adminUser = document.getElementsByClassName('wrapper__admin-users')[0];
+    todosUsuarios.forEach((arrayUsuario) => {
+        let divUsuario = document.createElement('div');
+        divUsuario.className = 'wrapper__admin-users-item';
+        let iconoUsuario = document.createElement('img');
+        iconoUsuario.src = 'img/loginProfile.png';
+        let divImagen = document.createElement('div');
+        divImagen.className = 'wrapper__admin-users-item-icon';
+        divImagen.appendChild(iconoUsuario);
+        divUsuario.appendChild(divImagen);
+        let usuarioID = arrayUsuario[0];
+        let divId = document.createElement('div');
+        divId.className = 'wrapper__admin-users-item-idUsuario';
+        divId.innerText = usuarioID;
+        divUsuario.appendChild(divId)
+        let usuarioNombre = arrayUsuario[1];
+        let divNombre = document.createElement('div');
+        divNombre.className="wrapper__admin-users-item-nombre";
+        divNombre.innerText = usuarioNombre;
+        divUsuario.appendChild(divNombre);
+        let usuarioApellido = arrayUsuario[2];
+        let divApellido = document.createElement('div');
+        divApellido.className="wrapper__admin-users-item-apellido";
+        divApellido.innerText = usuarioApellido;
+        divUsuario.appendChild(divApellido);
+        let usuarioDNI = arrayUsuario[3];
+        let divDNI = document.createElement('div');
+        divDNI.className = "wrapper__admin-users-item-dni";
+        divDNI.innerText = usuarioDNI;
+        divUsuario.appendChild(divDNI);
+        let arrayRoles = arrayUsuario[4];
+        let divRoles = document.createElement('div');
+        divRoles.className = "wrapper__admin-users-item-roles";
+        if(arrayRoles.length > 0){
+            let selectRoles = document.createElement('select');
+            arrayRoles.forEach((rol) => {
+                let option = document.createElement('option');
+                option.innerText = `${rol}`;
+                selectRoles.appendChild(option);
+            })
+            divRoles.appendChild(selectRoles);
+        } else {
+            divRoles.innerText = 'No tiene ningun rol asignado';
+        }
+        let divSelect = document.createElement('div');
+        let boton = document.createElement('button');
+        boton.innerText = 'Editar';
+        boton.id = usuarioID;
+        divSelect.className = "wrapper__admin-users-item-select";
+        divSelect.appendChild(boton);
+        divUsuario.appendChild(divSelect);
+        divUsuario.appendChild(divRoles);
+        adminUser.appendChild(divUsuario);
+    })
 }
 
-const recuperarUsuarios = () => {
-    fetch('localhost/AgriculturaPrecisionPHP/scripts/recuperarUsuario.php')
+function recuperarUsuarios(){
+    fetch('scripts/recuperarUsuario.php')
     .then((response) => {
         if(response.ok){
             return response.json();
@@ -16,8 +68,10 @@ const recuperarUsuarios = () => {
             return new Error('Fallo al intentar recuperar los usuarios');
         }
     })
-    .then((jsonUsuarios) => {
-        console.log(jsonUsuarios);
+    .then((todosUsuarios) => {
+        console.log(todosUsuarios);
+        todosUsuarios = todosUsuarios.usuarios[0];
+        crearUsuarios(todosUsuarios);
     })
     .catch((err) => {console.error(err);});
 }
