823ad225d274d913de587f01ddd5626ab4a9a97c
alexpg40 <alexpg40@educantabria.es>
2021-12-22 23:10:46 +0100

    empezando el metodo para recibir y crear las parcelas del usuario

--- javascript/panelParcelas.js
+++ javascript/panelParcelas.js
@@ -1,4 +1,19 @@
 
-const crearParcela = () => {
-    let parcelaDetails = document.createElement('details');
+const crearParcelas = (parcela) => {
+    
+}
+
+const recuperarParcelas = () => {
+    fetch("scripts/recuperarParcelas.php")
+    .then((response) => {
+        if(response.ok){
+            return response.json();
+        } else{
+            return new Error('Error en la petición al recuperar las parcelas');
+        }
+    })
+    .then((parcelas) => {
+        console.log(parcelas);
+        crearParcelas(parcelas);
+    })
 }
