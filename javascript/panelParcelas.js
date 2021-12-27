12206a164f35cbe037e86face90a9461d61be47f
alexpg40 <aperezg40@educantabria.es>
2021-12-27 23:11:10 +0100

    ya recupero y conseguido crear un mapa de la parcela segun sus puntos en la base de datos

--- javascript/panelParcelas.js
+++ javascript/panelParcelas.js
@@ -1,21 +1,28 @@
 
 const crearParcelas = (parcela) => {
-    let parcelasContainer = document.getElementsByClassName('parcela__items')
-    let divParcela = document.createElement('div');
-    let datosParcela = document.createElement('div');
+    let arrayParcelas = parcela.parcelas;
+    crearAreaParcela(arrayParcelas[0][4]);
+    let parcelasContainer = document.getElementsByClassName('parcela__item')
 }
 
 const recuperarParcelas = (idUsuario) => {
-    fetch("scripts/recuperarParcelas.php?idUsuario="+idUsuario)
-    .then((response) => {
-        if(response.ok){
-            return response.json();
-        } else{
-            return new Error('Error en la petición al recuperar las parcelas');
-        }
-    })
-    .then((parcelas) => {
-        console.log(parcelas);
-        crearParcelas(parcelas);
-    })
+    fetch("scripts/recuperarParcelas.php?idUsuario=" + idUsuario)
+        .then((response) => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                return new Error('Error en la petición al recuperar las parcelas');
+            }
+        })
+        .then((parcelas) => {
+            crearParcelas(parcelas);
+        })
+}
+
+const crearAreaParcela = (puntos) => {
+    let mapa = L.map('map').setView(puntos[0], 17);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(mapa);
+    let poligonoParcela = L.polygon(puntos, { color: 'green' }).addTo(mapa);
 }
