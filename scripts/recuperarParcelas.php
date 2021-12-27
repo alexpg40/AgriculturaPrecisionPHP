8d1122c1588d134a8213c010849cb177be84c80f
alexpg40 <aperezg40@educantabria.es>
2021-12-27 23:11:50 +0100

    quito un array innecesario a la hora de devolver las parcelas

--- scripts/recuperarParcelas.php
+++ scripts/recuperarParcelas.php
@@ -1,7 +1,7 @@
 <?php
 include '../basedatos/sesionBD.php';
 
-$usuarios['parcelas'][] = recuperarParcelas($_GET['idUsuario']);
+$usuarios['parcelas'] = recuperarParcelas($_GET['idUsuario']);
 
 echo json_encode($usuarios);
 ?>
