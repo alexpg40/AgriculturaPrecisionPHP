1446ee38b92e201e6c6e5d69134d1958a5fe0fe3
alexpg40 <aperezg40@educantabria.es>
2022-01-03 14:13:23 +0100

    funcion para recuperar los trabajos del script y crear la tabla con los trabajos

--- javascript/panelParcelas.js
+++ javascript/panelParcelas.js
@@ -1,7 +1,8 @@
-window.onload = () =>{
+window.onload = () => {
     let primerRadioButton = document.getElementsByName('seleccionar')[0];
-    primerRadioButton.setAttribute('checked', '')
-    recuperarParcela(primerRadioButton.value)
+    primerRadioButton.setAttribute('checked', '');
+    recuperarParcela(primerRadioButton.value);
+    recuperarTrabajos(primerRadioButton.value);
 }
 
 
@@ -16,8 +17,8 @@ const recuperarParcela = (idParcela) => {
             }
         })
         .then((parcelas) => {
-            console.log(parcelas.parcelas);
             crearAreaParcela(parcelas.parcelas[0]);
+            recuperarTrabajos(parcelas.parcelas[0]);
         })
 }
 
@@ -25,7 +26,7 @@ const crearAreaParcela = (parcela) => {
     let puntos = parcela[4];
     let divMap = document.getElementById('map');
     let mapContainer = document.getElementsByClassName('parcela__item')[0];
-    if(divMap != undefined){
+    if (divMap != undefined) {
         divMap.parentNode.removeChild(divMap);
     }
     let mapDiv = document.createElement('div');
@@ -33,9 +34,46 @@ const crearAreaParcela = (parcela) => {
     mapContainer.appendChild(mapDiv);
     let mapa = L.map('map').setView(puntos[0], 17);
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(mapa);
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(mapa);
     L.polygon(puntos, { color: 'green' }).addTo(mapa);
     let submitTrabajo = document.getElementsByName('programarParcela')[0];
     submitTrabajo.value = parcela[0];
+}
+
+const recuperarTrabajos = (idParcela) => {
+    fetch("scripts/recuperarTrabajos.php?idParcela=" + idParcela)
+        .then((response) => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                return new Error('No se pudo obtener los datos del servidor!');
+            }
+        })
+        .then((trabajos) => {
+            crearTablaTrabajos(trabajos);
+        })
+}
+
+const crearTablaTrabajos = (objeto) => {
+    let trabajos = objeto.trabajos;
+    let trabajosContainer = document.getElementsByClassName('parcela__trabajos__items')[0];
+    while(trabajosContainer.hasChildNodes()){
+        trabajosContainer.removeChild(trabajosContainer.childNodes[0]);
+    }
+    console.log(trabajosContainer);
+    trabajos.forEach(trabajo => {
+        let trabajoItem = document.createElement('div');
+        trabajoItem.className = "parcela__trabajos__item";
+        let divTipo = document.createElement('div');
+        divTipo.innerText = trabajo[0];
+        trabajoItem.appendChild(divTipo);
+        let divPiloto = document.createElement('div');
+        divPiloto.innerText = trabajo[1];
+        trabajoItem.appendChild(divPiloto);
+        let divFecha = document.createElement('div');
+        divFecha.innerText = trabajo[2];
+        trabajoItem.appendChild(divFecha);
+        trabajosContainer.appendChild(trabajoItem);
+    });
 }
