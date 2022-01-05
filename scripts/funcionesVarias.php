06f880b38b0df397c831b10729928a8d7519b0f0
alexpg40 <aperezg40@educantabria.es>
2022-01-05 20:31:58 +0100

    ahora la funcion de leerPuntosXML tambien lee la area, municipio y provincia de la parcela

--- scripts/funcionesVarias.php
+++ scripts/funcionesVarias.php
@@ -1,6 +1,6 @@
 <?php
 
-    function leerPuntosXML($ubicacion){
+    function leerPuntosXML(){
         $xml = simplexml_load_file("http://localhost/AgriculturaPrecisionPHP/ficheros/recintos/recintos.gml");
         $stringPuntos = $xml->featureMember->Recinto->geometry->Polygon->exterior->LinearRing->posList;
         $arrayPuntos = explode(' ', $stringPuntos);
@@ -10,7 +10,10 @@
             $long = $arrayPuntos[$i+1];
             array_push($puntos, [$long, $lat]);
         }
-        return $puntos;
+        $stringArea = $xml->featureMember->Recinto->dn_surface[0];
+        $stringMunicipio = $xml->featureMember->Recinto->municipio[0];
+        $stringProvincia = $xml->featureMember->Recinto->provincia[0];
+        return array($puntos, (string) $stringArea, (string) $stringMunicipio, (string) $stringProvincia);
     }
 
 ?>
