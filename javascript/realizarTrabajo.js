93ab54c11ba87970afd26b0c6af94fb80751f6dd
alexpg40 <aperezg40@educantabria.es>
2022-01-25 12:08:09 +0100

    funcion para dividir varias veces la linea

--- javascript/realizarTrabajo.js
+++ javascript/realizarTrabajo.js
@@ -26,5 +26,26 @@ const crearMapa = (parcela) => {
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(mapa);
-    L.polygon(puntos, { color: 'green' }).addTo(mapa);
+    let mapaParcela = L.polygon(puntos, { color: 'green' }).addTo(mapa);
+    let caja = L.rectangle(mapaParcela.getBounds(), { color: 'red' }).addTo(mapa);
+    let lineaArriba = [caja._latlngs[0][1], caja._latlngs[0][2]];
+    dividirRecta(mapa, lineaArriba)
+    lineaArriba.forEach(punto => { L.marker(punto).addTo(mapa); });
+}
+
+const crearPuntoMedio = (mapa, latlng1, latlng2) => {
+    const p1 = mapa.project(latlng1);
+    const p2 = mapa.project(latlng2);
+    return mapa.unproject(p1._add(p2)._divideBy(2));
+}
+
+const dividirRecta = (mapa, puntos) => {
+    for (let j = 0; j < 3; j++) {
+        let puntosNuevos = [];
+        for (let i = 1; i < puntos.length; i++) {
+            puntosNuevos.push(crearPuntoMedio(mapa, puntos[i-1], puntos[i]));
+        }
+        puntos.push(...puntosNuevos);
+        puntos.sort((a, b) => a.lng - b.lng);
+    }
 }
