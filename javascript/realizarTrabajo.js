927c719e777369c48ea470f433e4f1f96b9dd04e
alexpg40 <aperezg40@educantabria.es>
2022-01-30 22:06:34 +0100

    comentado el codigo para generar el mapa con las parcelas

--- javascript/realizarTrabajo.js
+++ javascript/realizarTrabajo.js
@@ -1,3 +1,4 @@
+/*Recupero la parcela haciendo un fetch a un script php*/
 const recuperarParcela = (idParcela) => {
     fetch("scripts/recuperarParcelas.php?idParcela=" + idParcela)
         .then((response) => {
@@ -12,43 +13,71 @@ const recuperarParcela = (idParcela) => {
         })
 }
 
+/*Creo el mapa con la parcela y la ruta del dron */
 const crearMapa = (parcela) => {
+    //Recupero los puntos de la parcela
     let puntos = parcela[4];
+    
+    //Elimino el mapa si existe
     let divMap = document.getElementById('map');
     let mapContainer = document.getElementsByClassName('mapContainer')[0];
     if (divMap != undefined) {
         divMap.parentNode.removeChild(divMap);
     }
+
+    //Creo el div donde se creara el mapa
     let mapDiv = document.createElement('div');
     mapDiv.id = "map";
     mapContainer.appendChild(mapDiv);
+
+    //Creo el mapa
     let mapa = L.map('map').setView(puntos[0], 17);
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(mapa);
+
+    //Dibujo la parcela en el mapa
     let mapaParcela = L.polygon(puntos, { color: 'green' }).addTo(mapa);
+
+    //Dibujo la caja que contiene a la parcela
     let caja = L.rectangle(mapaParcela.getBounds(), { color: 'red' }).addTo(mapa);
+
+    //Consigo las lineas de arriba y de abajo del cuadrado
     let lineaArriba = [caja._latlngs[0][1], caja._latlngs[0][2]];
     let lineaAbajo = [caja._latlngs[0][3], caja._latlngs[0][0]];
+
+    //Divido esas lineas entre varios puntos por donde pasara el polyline
     dividirRecta(mapa, lineaArriba);
     dividirRecta(mapa, lineaAbajo);
+
+    //Polyline de la ruta en el cuadrado 
     let polylinePath = ordenarPolypath(lineaArriba, lineaAbajo);
-    let polyline = L.polyline(polylinePath, {color: 'white'}).addTo(mapa);
-    let coordenadas1 = polyline._latlngs.map(({lat, lng}) => [lat, lng]);
-    let coordenadas2 = mapaParcela._latlngs.map(({lat, lng}) => [lat, lng]);
+    let polyline = L.polyline(polylinePath, {color: 'white'});
+
+    //Consigo los puntos de interseccion de la parcela con el polyline del cuadrado
     var intersects = turf.lineIntersect(polyline.toGeoJSON(), mapaParcela.toGeoJSON());
+
+    //Consigo las coordenadas del objeto de la interseccion
     let coordenadasGeoJSON = intersects.features.map(({geometry}) => geometry.coordinates);
+
+    //Convierto las coordenadas en un formato para usarlos en Leaflet
     let coordenadasFinales = coordenadasGeoJSON.map((coordenada) => L.GeoJSON.coordsToLatLng(coordenada));
+
+    //Ordeno las coordenadas para dibujar el polyline de la parcela
     coordenadasFinales = ordenarPolypathInterior(coordenadasFinales);
+
+    //Dibujo el polyline de la parcela en el mapa
     L.polyline(coordenadasFinales, {color: 'black'}).addTo(mapa);
 }
 
+//Consigo el punto medio de una recta
 const crearPuntoMedio = (mapa, latlng1, latlng2) => {
     const p1 = mapa.project(latlng1);
     const p2 = mapa.project(latlng2);
     return mapa.unproject(p1._add(p2)._divideBy(2));
 }
 
+//Divido los puntos de una recta varias veces
 const dividirRecta = (mapa, puntos) => {
     for (let j = 0; j < 5; j++) {
         let puntosNuevos = [];
@@ -60,6 +89,7 @@ const dividirRecta = (mapa, puntos) => {
     }
 }
 
+//Ordena los puntos de ambas lineas para crear el polyline 
 const ordenarPolypath = (lineaArriba, lineaAbajo) =>{
     let polyPath = [];
     polyPath.push(lineaAbajo[0]);
@@ -73,6 +103,7 @@ const ordenarPolypath = (lineaArriba, lineaAbajo) =>{
     return polyPath;
 }
 
+//Ordena los puntos para crear el polypath interior
 const ordenarPolypathInterior = (coordenadas) => {
     let ret = [];
     ret.push(coordenadas[0]);
